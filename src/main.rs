015ef10fa25ad536dccf98971ed213fa06331835
becky112358 <8b2@protonmail.com>
2020-07-09 15:49:50 +0300

    Code to compute the shape of a plane, which, when multiple are combined, will create an approximate 3d parabola.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::f64::consts::FRAC_PI_2;
+
+const LOWER_RADIUS: f64 = 0.0;
+const HEATING_RADIUS: f64 = 0.4;
+const UPPER_RADIUS: f64 = 0.6;
+const HEATING_HEIGHT: f64 = 0.2;
+const N_PANELS: i32 = 12;
+
+fn truncate(long: f64) -> f64 {
+    let bigger = long * 100.0;
+    let integer = bigger.round() as i32;
+    integer as f64 / 100.0
 }
+
+fn length(b: f64, x: f64) -> f64 {
+    let u_squared = 1.0 + (4.0 * b * b * x * x);
+    let u = u_squared.sqrt();
+
+    if u < 1.0 {
+        println!("***** u is less than 1 *****");
+    }
+
+    (1.0 / (4.0 * b)) * (u.acosh() + ((u.acosh().sinh()) * u))
+}
+
+fn main() {
+
+    // p is the heating height above zero
+    let p = ((HEATING_RADIUS * HEATING_RADIUS) - (LOWER_RADIUS * LOWER_RADIUS))
+          / (4.0 * HEATING_HEIGHT);
+
+    // y = b x^2
+    let b = 1.0 / (4.0 * p);
+
+    println!("p {}, b {}", p, b);
+
+
+    let angle_sum = (N_PANELS as f64 - 2.0) * FRAC_PI_2;
+    let half_angle = angle_sum / (2.0 * N_PANELS as f64);
+
+    println!("half angle {}", half_angle);
+
+
+    let invisible_length = length(b, LOWER_RADIUS);
+
+
+    let increment_size = (UPPER_RADIUS - LOWER_RADIUS) / 200.0;
+    let mut x = LOWER_RADIUS;
+
+
+    while x <= UPPER_RADIUS {
+        let t = length(b, x) - invisible_length;
+
+        let w = (2.0 * x) / (half_angle.tan());
+
+        println!("x {:>width$}   length {:>width$}   width {:>width$}",
+            truncate(x), truncate(t), truncate(w), width=6);
+
+        x += increment_size;
+    }
+}
+
