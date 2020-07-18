8fa5829241fb01dbb28077ba659e5d1debc9d1b3
becky112358 <8b2@protonmail.com>
2020-07-18 16:22:40 +0300

    Add a second method for building an approximate paraboloid. Take horizontal slices of cones of varying gradients.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use std::f64::consts::PI;
 
 const LOWER_RADIUS_M: f64 = 0.0;
-const UPPER_RADIUS_M: f64 = 0.6;
+const UPPER_RADIUS_M: f64 = 1.0;
 // Heating height above zero.
 // Note that if the lower radius is not zero, then the parabola will start
 // above zero.
-const HEATING_HEIGHT_M: f64 = 0.2;
+const HEATING_HEIGHT_M: f64 = 0.5;
 const N_PANELS: i32 = 12;
 
 fn main() {
     let b = parabola_variable(HEATING_HEIGHT_M);
 
+    method_parabola_extended_to_a_plane(b);
+
+    method_cone_rings(b);
+}
+
+fn parabola_variable(focal: f64) -> f64 {
+    // y = b x^2
+    let b = 1.0 / (4.0 * focal);
+    println!("p {}, b {}", focal, b);
+    return b;
+}
+
+fn method_parabola_extended_to_a_plane(b: f64) {
     let half_angle = half_angle_of_regular_n_sided_polygon(N_PANELS);
 
-    let invisible_length = length(b, LOWER_RADIUS_M);
+    let invisible_length = parabola_length(b, LOWER_RADIUS_M);
 
     iterate_over_x(
         LOWER_RADIUS_M,
@@ -21,13 +34,6 @@ fn main() {
         b,
         half_angle,
         invisible_length);
-}
-
-fn parabola_variable(focus_point: f64) -> f64 {
-    // y = b x^2
-    let b = 1.0 / (4.0 * focus_point);
-    println!("p {}, b {}", focus_point, b);
-    return b;
 }
 
 fn half_angle_of_regular_n_sided_polygon(n_sides: i32) -> f64 {
@@ -48,11 +54,11 @@ fn iterate_over_x(
     let mut x = x_start;
 
     while x <= x_finish {
-        let t = length(b, x) - invisible_length;
+        let t = parabola_length(b, x) - invisible_length;
 
         let w = (2.0 * x) / (half_angle.tan());
 
-        let y = b * x * x;
+        let y = parabola(b, x);
 
         println!("x {:>width$.ndp$}   length {:>width$.ndp$}   width {:>width$.ndp$}   height {:>width$.ndp$}",
             x, t, w, y, width=6, ndp=3);
@@ -61,7 +67,7 @@ fn iterate_over_x(
     }
 }
 
-fn length(b: f64, x: f64) -> f64 {
+fn parabola_length(b: f64, x: f64) -> f64 {
     let u_squared = 1.0 + (4.0 * b * b * x * x);
     let u = u_squared.sqrt();
 
@@ -70,5 +76,68 @@ fn length(b: f64, x: f64) -> f64 {
     }
 
     (1.0 / (4.0 * b)) * (u.acosh() + ((u.acosh().sinh()) * u))
+}
+
+fn method_cone_rings(b: f64) {
+    let mut x0 = 0.05;
+    println!("Base circle circumference {:.ndp$}", x0, ndp=3);
+
+    while x0 < UPPER_RADIUS_M {
+        let x1 = choose_x(b, x0);
+
+        let y0 = parabola(b, x0);
+        let y1 = parabola(b, x1);
+        let length = hypotenuse_length_squared(x0, x1, y0, y1).sqrt();
+
+        let x0_circumference = circumference(x0);
+        let x1_circumference = circumference(x1);
+
+        println!("Lower circumference {:>width$.ndp$}   Upper circumference {:>width$.ndp$}   Length {:>width$.ndp$}",
+            x0_circumference, x1_circumference, length, width=6, ndp=3);
+
+        x0 = x1;
+    }
+}
+
+fn choose_x(b: f64, x0: f64) -> f64 {
+    let focal_radius : f64 = 0.1;
+    let focal_radius_squared = focal_radius.powf(2.0);
+    let tolerance = 0.0015;
+
+    let y0 = parabola(b, x0);
+
+    let x_max = x0 + focal_radius;
+
+    let mut x1 = x_max;
+    let mut y1 = parabola(b, x1);
+
+    let mut current_target_squared = hypotenuse_length_squared(x0, x1, y0, y1);
+
+    while !(current_target_squared < (focal_radius_squared + tolerance)
+        && current_target_squared > (focal_radius_squared - tolerance)) {
+        if current_target_squared > (focal_radius_squared + tolerance) {
+            x1 = x0 + ((x1 - x0) * 0.5);
+        }
+        else {
+            x1 = x_max - ((x_max - x1) * 0.5);
+        }
+
+        y1 = parabola(b, x1);
+        current_target_squared = hypotenuse_length_squared(x0, x1, y0, y1);
+    }
+
+    return x1;
+}
+
+fn circumference(radius: f64) -> f64 {
+    2.0 * PI * radius
+}
+
+fn hypotenuse_length_squared(x0: f64, x1: f64, y0: f64, y1: f64) -> f64 {
+    (x1 - x0).powf(2.0) + (y1 - y0).powf(2.0)
+}
+
+fn parabola(b: f64, x: f64) -> f64 {
+    b * x * x
 }
 
