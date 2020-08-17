6628970d8f39aeee63b5e6ae753d27e526ea59ad
becky112358 <8b2@protonmail.com>
2020-08-17 16:09:11 +0300

    Add documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 
+/// The equation of a parabola is y = b x^2.
+/// The relationship between b and the focal of the parabola is
+/// b = 1.0 / (4.0 * focal)
 pub fn parabola_variable(focal: f64) -> f64 {
     // y = b x^2
     let b = 1.0 / (4.0 * focal);
